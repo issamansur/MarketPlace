7f1ba41b5aea9144a87ca06cdde7beb171c261ab
Issa <issa_mansur@mail.ru>
2024-05-13 01:54:31 +0300

    Rollback ^1

--- MarketPlace.Application/Common/ICrudRepository.cs
+++ MarketPlace.Application/Common/ICrudRepository.cs
@@ -4,7 +4,7 @@ public interface ICrudRepository<T>
 {
     Task<Guid> CreateAsync(T entity, CancellationToken cancellationToken);
     Task UpdateAsync(T entity, CancellationToken cancellationToken);
-    Task DeleteAsync(Guid entityId, CancellationToken cancellationToken);
+    Task DeleteAsync(T entity, CancellationToken cancellationToken);
     
     Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 }
